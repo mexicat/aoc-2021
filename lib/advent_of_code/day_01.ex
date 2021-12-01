01ebfd93c0e190e2598cb82feb8613bb91d82c00
Giacomo Magnanini <gikappa@gmail.com>
2021-12-01 10:18:50 +0100

    day 1 refactor

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -2,24 +2,21 @@ defmodule AdventOfCode.Day01 do
   def part1(input) do
     input
     |> parse_input()
-    |> Enum.reduce({0, 0}, fn x, {curr, tot} ->
+    |> Enum.reduce({:infinity, 0}, fn x, {curr, tot} ->
       {x, if(x > curr, do: tot + 1, else: tot)}
     end)
     |> elem(1)
-    |> Kernel.-(1)
   end
 
   def part2(input) do
-    numbers = input |> parse_input()
-    numbers_2 = numbers |> Enum.drop(1)
-    numbers_3 = numbers_2 |> Enum.drop(1)
-
-    Enum.zip_reduce([numbers, numbers_2, numbers_3], {0, 0}, fn ns, {curr, tot} ->
+    input
+    |> parse_input()
+    |> Enum.chunk_every(3, 1)
+    |> Enum.reduce({:infinity, 0}, fn ns, {curr, tot} ->
       n_tot = Enum.sum(ns)
       {n_tot, if(n_tot > curr, do: tot + 1, else: tot)}
     end)
     |> elem(1)
-    |> Kernel.-(1)
   end
 
   def parse_input(input) do
